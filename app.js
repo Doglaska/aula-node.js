4075580a68c4363731c8ac1d8b9a7125c5d017d7
Doglas <doglaska@gmail.com>
2024-02-23 10:36:28 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var adicao = require('./adicao')
+var sub = require('./sub')
+var mult = require ('./mult')
+var divisao = require('./divisao')
+
+
+console.log(adicao(10,2))
+console.log(sub(10,2))
+console.log(mult(10,2))
+console.log(divisao(10,2))
+
+    
---
+++ mult.js
@@ -0,0 +1,4 @@
+var mult = function(n1, n2){
+    return n1 * n2
+}
+module.exports = mult
